af252c4282371030e4d97e3e3ef7d3813afb84c6
kes8842 <75794083+kes8842@users.noreply.github.com>
2020-12-15 15:43:35 +0900

    Update index.js

--- store/reducers/index.js
+++ store/reducers/index.js
@@ -1,8 +1,6 @@
 import { combineReducers } from "redux";
-import { postReducer } from "./postReducer";
 import { translateReducer } from "./translate/translateReducer";
 
 export default combineReducers({
-  post: postReducer,
   translate: translateReducer,
 });
