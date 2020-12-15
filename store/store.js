dca87cdae132a90ba74c40e9f18082a0fbf6df6d
kes8842 <75794083+kes8842@users.noreply.github.com>
2020-12-15 15:31:45 +0900

    Create store.js

---
+++ store/store.js
@@ -0,0 +1,25 @@
+import { createStore, applyMiddleware } from "redux";
+import { createWrapper } from "next-redux-wrapper";
+import thunk from "redux-thunk";
+import { composeWithDevTools } from "redux-devtools-extension";
+import createSagaMiddleware from "redux-saga";
+import rootReducer from "./reducers";
+import rootSaga from "../sagas";
+
+const configureStore = (initialState, options) => {
+  const sagaMiddleware = createSagaMiddleware(); // 리덕스 사가 생성
+  const middlewares = [sagaMiddleware]; // 미들웨어 연결
+  const enhancer =
+    process.env.NODE_ENV === "production"
+      ? compose(applyMiddleware(...middlewares))
+      : composeWithDevTools(applyMiddleware(...middlewares));
+  const store = createStore(rootReducer, initialState, enhancer); // enhancer에 넣어서 saga가 적용된 store 생성
+  store.sagaTask = sagaMiddleware.run(rootSaga); // store에 rootSaga를 넣은 sagaMiddleware를 실행시켜준다.
+  return store;
+};
+
+const wrapper = createWrapper(configureStore, {
+  debug: process.env.NODE_ENV === "development,",
+});
+
+export default wrapper;
