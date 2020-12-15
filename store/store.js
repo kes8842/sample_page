d5ff0ffcdc6799bc2be062c70f2cb9c1d7b3f876
kes8842 <75794083+kes8842@users.noreply.github.com>
2020-12-15 15:42:33 +0900

    Update store.js

--- store/store.js
+++ store/store.js
@@ -1,6 +1,5 @@
 import { createStore, applyMiddleware } from "redux";
 import { createWrapper } from "next-redux-wrapper";
-import thunk from "redux-thunk";
 import { composeWithDevTools } from "redux-devtools-extension";
 import createSagaMiddleware from "redux-saga";
 import rootReducer from "./reducers";
