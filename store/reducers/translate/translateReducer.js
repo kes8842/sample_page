0958a360b3446d718fb3e275562cca84161b3432
kes8842 <75794083+kes8842@users.noreply.github.com>
2020-12-15 14:59:56 +0900

    Create translateReducer.js

---
+++ store/reducers/translate/translateReducer.js
@@ -0,0 +1,38 @@
+import action from "../../action";
+import { UPDATE_RESULT } from "../../action/translate/translateAction";
+
+const { UPDATE_SOURCE, UPDATE_TARGET, CALL_TRANS_API } = action.transAction;
+
+const initState = {
+  transResult: "",
+  source: "ko",
+  target: "en",
+  options: { ko: "한국", en: "영어", ja: "일본어" },
+};
+
+export const translateReducer = (state = initState, action) => {
+  switch (action.type) {
+    case UPDATE_SOURCE:
+      return {
+        ...state,
+        source: action.source,
+      };
+    case UPDATE_TARGET:
+      return {
+        ...state,
+        target: action.target,
+      };
+
+    case CALL_TRANS_API:
+      return {
+        ...state,
+      };
+    case UPDATE_RESULT:
+      return {
+        ...state,
+        transResult: action.value?.value,
+      };
+    default:
+      return state;
+  }
+};
